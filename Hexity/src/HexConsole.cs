8e97920d2d337753a6be9a483482891ac45655e6
Jake Sutton <jrsutton@ucsd.edu>
2016-08-03 20:58:22 -0700

    Paving the road for declaration of properties for pools.

--- Hexity/src/HexConsole.cs
+++ Hexity/src/HexConsole.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hexity.Engines;
 
 namespace HexCommands
 {
+
     interface IRunnable
     {
     	bool Run(string[] parameters);
 		string Help();
-
     }
+
+	public static class Manager 
+	{
+		public static bool Responding;
+		public static string CurrentPool;
+		public static Dictionary<string, ObjectPool> State;
+	}
 
 	class Create : IRunnable
 	{
@@ -18,9 +28,55 @@ namespace HexCommands
 
 		public bool Run(string[] parameters)
 		{
+			if (parameters.Length > 2)
+			{
 
+				int parameterStart = -1;
+				int parameterEnd = -1;
+				for (int i = 0; i < parameters.Length; ++i)
+				{
+					if (parameters[i].Contains("{")) 
+					{
+						parameterStart = i;
+						break;
+					}
+				}
 
-			throw new NotImplementedException();
+				for (int i = parameterStart - 1; i < parameters.Length; ++i)
+				{
+					if (parameters[i].Contains("}"))
+					{
+						parameterEnd = i;
+						break;
+					}
+				}
+
+				string[] data = parameters.Skip(parameterStart).Take(parameterEnd - (parameterStart - 1)).ToArray();
+
+				string final = string.Join("", data);
+
+				Console.WriteLine("{0}", final);
+
+				// if the following argument does not start with {
+				for (int i = 1; i < parameters.Length; ++i)
+				{
+					CreateClean(parameters[i]);
+				}
+			}
+
+			return true;
+		}
+
+		void CreateClean( string name ) 
+		{
+			var objectPool = new ObjectPool();
+			Manager.State.Add(name, objectPool);
+			Manager.CurrentPool = name;
+		} 
+
+		void CreateWithProperties( string name, string[] properties )
+		{
+			// where we can implement a setter for properties
 		}
 	}
 
@@ -33,7 +89,31 @@ namespace HexCommands
 
 		public bool Run(string[] parameters)
 		{
-			throw new NotImplementedException();
+			string toDelete = parameters[1];
+
+			if (toDelete == Manager.CurrentPool)
+			{
+				Console.WriteLine("Cannot delete the currently open object pool.");
+
+				return true;
+			}
+
+			if (Manager.State[toDelete].Count() > 0)
+			{
+				Console.WriteLine("{0} contains objects. Are you sure?", toDelete);
+
+				string conf = Console.ReadLine();
+				if (conf == "y")
+				{
+					Manager.State.Remove(toDelete);
+				}
+			}
+			else
+			{
+				Manager.State.Remove(toDelete);
+			}
+
+			return true;
 		}
 	}
 
@@ -46,7 +126,13 @@ namespace HexCommands
 
 		public bool Run(string[] parameters)
 		{
-			throw new NotImplementedException();
+			for (int i = 1; i < parameters.Length; ++i)
+			{
+				var eng = new ObjectEngine(parameters[i]);
+				Manager.State[Manager.CurrentPool].AddObject(eng);
+			}
+
+			return true;
 		}
 	}
 
@@ -59,7 +145,120 @@ namespace HexCommands
 
 		public bool Run(string[] parameters)
 		{
+			string toRemove = parameters[1];
+
+			List<ObjectEngine> engTemp = new List<ObjectEngine>() { };
+			foreach (var item in Manager.State[Manager.CurrentPool].GetObjects())
+			{
+				if (item.Hex.Name == toRemove)
+				{
+					engTemp.Add(item);
+				}
+			}
+
+			foreach (var del in engTemp)
+			{
+				Manager.State[Manager.CurrentPool].RemoveObject(del);
+			}
+
+			return true;
+		}
+	}
+
+	class List : IRunnable
+	{
+		public string Help()
+		{
 			throw new NotImplementedException();
+		}
+
+		public bool Run(string[] parameters)
+		{
+			Console.WriteLine(Manager.CurrentPool);
+			foreach (var item in Manager.State[Manager.CurrentPool].GetObjects())
+			{
+				Console.WriteLine("* " + item.Hex.Name);
+			}
+
+			return true;
+		}
+	}
+
+	class Pools : IRunnable
+	{
+		public string Help()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Run(string[] parameters)
+		{
+			Console.WriteLine("* " + Manager.CurrentPool);
+			foreach (var item in Manager.State.Keys)
+			{
+				if (!item.Equals(Manager.CurrentPool))
+					Console.WriteLine(item);
+			}
+
+			return true;
+		}
+	}
+
+	class Open : IRunnable
+	{
+		public string Help()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Run(string[] parameters)
+		{
+			Manager.CurrentPool = parameters[1];
+
+			return true;
+		}
+	}
+
+	class Link : IRunnable
+	{
+		public string Help()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Run(string[] parameters)
+		{
+			string first = parameters[1];
+			string second = parameters[3];
+			string linkType = parameters[2];
+
+			if (linkType.Equals("<->"))
+			{
+				foreach (var item in Manager.State[first].GetObjects())
+				{
+					if (Manager.State[second].Contains(item.Hex.Name))
+					{
+						Console.WriteLine(item.Hex.Name);
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+
+	class Q : IRunnable
+	{
+		public string Help()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Run(string[] parameters)
+		{
+			Manager.Responding = false;
+
+			return true;
 		}
 	}
 }
